a7d6697f5a5bc3677830ee3d84ce0811a1a63dec
Brendan Wood <brendan.wood@concordia.ca>
2021-09-16 18:34:36 -0400

    Git Demo Class 2

--- src/ca/concordia/cejv416/fall2021/Class_01/HelloWorld.java
+++ src/ca/concordia/cejv416/fall2021/Class_01/HelloWorld.java
@@ -3,6 +3,7 @@ package ca.concordia.cejv416.fall2021.Class_01;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("------------");
     }
 }
---
+++ src/ca/concordia/cejv416/fall2021/Class_02/GitAddExample.java
@@ -0,0 +1,11 @@
+package ca.concordia.cejv416.fall2021.Class_02;
+
+public class GitAddExample {
+
+    public static void main(String[] args) {
+
+        System.out.println("A");
+
+    }
+
+}
